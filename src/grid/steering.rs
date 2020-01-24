12dc4a49bc81c680744d9c50d5e8e3cba14d3284
Mark LeMoine <thatsgobbles@gmail.com>
2020-01-23 23:40:19 -0800

    Cleanup and doc comments in `steering`

--- src/grid/steering.rs
+++ src/grid/steering.rs
@@ -1,3 +1,4 @@
+/// Represents each of the ways that a path can exit the center of a character cell.
 #[derive(Copy, Clone)]
 pub enum Steering {
     Straight,
@@ -6,11 +7,12 @@ pub enum Steering {
 }
 
 impl Steering {
-    pub fn flip(&self) -> Self {
+    /// Inverts this `Steering`, such that left and right are reversed.
+    pub fn invert(&self) -> Self {
         match self {
-            &Steering::Left => Steering::Right,
-            &Steering::Right => Steering::Left,
-            &Steering::Straight => Steering::Straight,
+            &Self::Left => Self::Right,
+            &Self::Right => Self::Left,
+            &Self::Straight => Self::Straight,
         }
     }
 }
